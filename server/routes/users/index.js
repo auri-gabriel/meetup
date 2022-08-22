fc4a66722ff1afb47883892d771a65d10585e0f4
Auri Gabriel <ririmello.a@gmail.com>
2022-08-22 01:35:21 -0300

    logout

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -15,6 +15,11 @@ module.exports = () => {
   router.get('/login', (req, res) =>
     res.render('users/login', { error: req.query.error })
   );
+
+  router.get('/logout', (req, res) => {
+    req.logout({}, (err) => console.log(err));
+    return res.redirect('/');
+  });
   router.get('/registration', (req, res) =>
     res.render('users/registration', { success: req.query.success })
   );
