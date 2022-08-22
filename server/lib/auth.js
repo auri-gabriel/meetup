cd36dfc68da950c1981af0ede952bbb75d46a9f2
Auri Gabriel <ririmello.a@gmail.com>
2022-08-22 01:10:58 -0300

    authentication strategy

--- server/lib/auth.js
+++ server/lib/auth.js
@@ -1,4 +1,35 @@
+const { use } = require('chai');
 const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const UserModel = require('../models/UserModel');
+
+passport.use(
+  new LocalStrategy(
+    { usernameField: 'email' },
+    async (username, password, done) => {
+      try {
+        const user = await UserModel.findOne({ email: username }).exec();
+
+        if (!user) {
+          return done(null, false, {
+            message: ' Invalid username or password',
+          });
+        }
+        const passwordOk = await user.comparePassword(password);
+
+        if (!passwordOk) {
+          return done(null, false, {
+            message: ' Invalid username or password',
+          });
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
 
 module.exports = {
   initialize: passport.initialize(),
